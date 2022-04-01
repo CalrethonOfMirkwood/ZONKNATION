d4617a66cff7b7c6bf284b12d311f35a184e2963
CalrethilOfMirkwood <82109882+CalrethilOfMirkwood@users.noreply.github.com>
2022-04-01 15:13:04 -0700

    more base files

--- master.py
+++ master.py
@@ -3,8 +3,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/calender')
+def calender():
+    return render_template('calender.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# error handling
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
