7b66d57f6b0bcb43c543a8e051b8184739110fb2
CalrethonOfMirkwood <82109882+CalrethilOfMirkwood@users.noreply.github.com>
2022-05-23 14:43:46 -0700

    my phone number is 9 digits long.

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from __init__ import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
-    phonenumber = db.Column(db.String(9), unique=True)
+    phonenumber = db.Column(db.String(10), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     pronouns = db.Column(db.String(1000))
